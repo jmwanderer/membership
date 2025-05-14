4a07cd93f54f3389ae8acf1c41bdc5ad52678e48
jim <jmwanderer@gmail.com>
2025-05-13 17:22:33 -0700

    PDF.py takes a filename

--- pdf.py
+++ pdf.py
@@ -1,12 +1,17 @@
+import sys
+
 import pdfplumber
 
-def main():
-    with pdfplumber.open("guest.pdf") as pdf:
+def main(filename: str):
+    with pdfplumber.open(filename) as pdf:
         for page in pdf.pages:
             text = page.extract_text_simple()
             print(text)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("usage: pdf <filename>")
+        sys.exit(-1)
+    main(sys.argv[1])
 
 
